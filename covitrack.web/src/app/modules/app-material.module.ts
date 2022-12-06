41647661b9e7e8bb6e8899c70e2a41324f49852d
IamTheLegend <vivekaprasanthannam@gmail.com>
2022-12-05 19:08:42 -0500

    Added an API for raw data and plugged in API with 4 widgets

--- covitrack.web/src/app/modules/app-material.module.ts
+++ covitrack.web/src/app/modules/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [],
@@ -13,14 +14,16 @@ import { MatSelectModule } from '@angular/material/select';
     MatToolbarModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSlideToggleModule
   ]
 })
 export class AppMaterialModule { }
